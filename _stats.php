56ab5637986d677a086a1d262d52d7294670150d
MrBeardedJoe <goneover@gmail.com>
2024-05-31 15:03:26 -0500

    moved stats and corrected typo

--- _stats.php
+++ _stats.php
@@ -4,22 +4,21 @@ $jsonData = openFile($filePath);
 $totalCount = count($jsonData['codes']);
 $totalNotChecked = 0;
 $creditedCount = 0;
-$creditersCount = [];
-$uniqueCrediters = [];
+$creditorsCount = [];
+$uniqueCreditors = [];
 
 foreach ($jsonData['codes'] as $data) {
-
   if ($data['status'] == "invalid") $totalNotChecked++;
   if ($data['credit'] != "") {
     $creditedCount++;
-    $creditersCount[$data['credit']] = $data['credit'];
+    $creditorsCount[$data['credit']] = $data['credit'];
+    if (!in_array($data['credit'], $uniqueCreditors)) array_push($uniqueCreditors, $data['credit']);
   }
-  if ($data['credit'] != "" && !in_array($data['credit'], $uniqueCrediters)) array_push($uniqueCrediters, $data['credit']);
 }
 
-$crediters = '';
-foreach ($uniqueCrediters as $uniqueCrediter) $crediters .= "$uniqueCrediter ";
-$creditersCount = count($creditersCount);
+$creditors = '';
+foreach ($uniqueCreditors as $uniqueCreditor) $creditors .= "$uniqueCreditor ";
+$creditorsCount = count($creditorsCount);
 
 $randomCode = array_rand($jsonData['codes'], 1);
 
@@ -47,43 +46,40 @@ echo "
       <div class='card border border-primary bg-dark text-white'>
         <h5 class='card-header bg-primary text-white p-1'>Stats</h5>
         <div class='card-body p-2'>
-
           <div class='row'>
 
-          <div class='col'>
-            <div class='card border-secondary'>
-            <h6 class='card-header p-1 bg-secondary text-white text-center'>Codes Done</h6>
-            <div class='card-body p-1'>
-            Invalid: <b>$totalNotChecked</b> of $totalCount ($invalidPercent)<BR>
-            Credited: <b>$creditedCount</b> of $totalCount ($creditedPercent)<BR> 
-            Total Codes: <b>$totalCount</b>
-            </div>
-            </div>
-          </div>
-
-          <div class='col'>
-            <div class='card border-secondary'>
-              <h6 class='card-header p-1 bg-secondary text-white text-center'>Credits Go To</h6>
-              <div class='card-body p-1'>
-                <b>$creditersCount</b> Users have helped crack the vault!<hr class='bg-primary my-0'>
-                <small>$crediters</small>
+            <div class='col'>
+              <div class='card border-secondary'>
+                <h6 class='card-header p-1 bg-secondary text-white text-center'>Digit Info</h6>
+                <div class='card-body p-1'>
+                  Unique Numbers: <b>$uniqueCount</b> <BR>
+                  Code Length: <b>$digitsNeededCount</b> <BR>
+                  Numbers Needed: <b>$uniqueDigitsList</b>
+                </div>
               </div>
             </div>
-          </div>
 
-
-          <div class='col'>
-            <div class='card border-secondary'>
-              <h6 class='card-header p-1 bg-secondary text-white text-center'>Digit Info</h6>
-              <div class='card-body p-1'>
-                Unique Numbers: <b>$uniqueCount</b> <BR>
-                Code Length: <b>$digitsNeededCount</b> <BR>
-                Numbers Needed: <b>$uniqueDigitsList</b>
-
+            <div class='col'>
+              <div class='card border-secondary'>
+                <h6 class='card-header p-1 bg-secondary text-white text-center'>Codes Done</h6>
+                <div class='card-body p-1'>
+                  Invalid: <b>$totalNotChecked</b> of $totalCount ($invalidPercent)<BR>
+                  Credited: <b>$creditedCount</b> of $totalCount ($creditedPercent)<BR> 
+                  Total Codes: <b>$totalCount</b>
+                </div>
               </div>
             </div>
-          </div>
 
+            <div class='col'>
+              <div class='card border-secondary'>
+                <h6 class='card-header p-1 bg-secondary text-white text-center'>Credits Go To</h6>
+                <div class='card-body p-1'>
+                  <b>$creditorsCount</b> Users have helped crack the vault!<hr class='bg-primary my-0'>
+                  <small>$creditors</small>
+                </div>
+              </div>
+            </div>
+            
           </div>
         </div>
       </div>
