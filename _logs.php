007061a078e44f90878078aa63e0449c9974953e
MrBeardedJoe <goneover@gmail.com>
2024-05-31 12:54:04 -0500

    less padding/margin on logs

--- _logs.php
+++ _logs.php
@@ -3,13 +3,13 @@ echo "
 <div class='row'>
   <div class='col-12 py-1 px-4'>
     <div class='card border border-info bg-dark text-white'>
-      <h5 class='card-header bg-info text-white'>Logs</h5>
-      <div class='card-body'>
+      <h5 class='card-header bg-info text-white p-1'>Logs</h5>
+      <div class='card-body p-1'>
       ";
 
-        foreach ($jsonData['logs'] as $key => $log) echo "<p>{$log[0]}</p>";
+foreach ($jsonData['logs'] as $key => $log) echo "<p class='my-1'>{$log[0]}</p>";
 
-        echo "
+echo "
       </div>
     </div>
   </div>
