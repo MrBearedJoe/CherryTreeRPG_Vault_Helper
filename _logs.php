3ea2bb4c93b79330e2003939bde1a1c25fa5e1f8
MrBeardedJoe <goneover@gmail.com>
2024-05-31 12:53:02 -0500

    cleaning

--- _logs.php
+++ _logs.php
@@ -1,18 +1,17 @@
 <?php
 echo "
 <div class='row'>
-<div class='col-12 py-1 px-4'>
+  <div class='col-12 py-1 px-4'>
+    <div class='card border border-info bg-dark text-white'>
+      <h5 class='card-header bg-info text-white'>Logs</h5>
+      <div class='card-body'>
+      ";
 
-<div class='card border border-info bg-dark text-white'>
-<h5 class='card-header bg-info text-white'>Logs</h5>
-<div class='card-body'>
-";
-foreach ($jsonData['logs'] as $key => $log) {
-  echo "<p>{$log[0]}</p>";
-}
-echo "
-</div>
-</div>
-</div>
+        foreach ($jsonData['logs'] as $key => $log) echo "<p>{$log[0]}</p>";
+
+        echo "
+      </div>
+    </div>
+  </div>
 </div>
 ";
