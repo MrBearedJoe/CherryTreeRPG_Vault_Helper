db5dfb53102abc64e9cc0e5703503a14827f9551
MrBeardedJoe <goneover@gmail.com>
2024-05-31 14:57:19 -0500

    hide logs by default. click header to show

--- _logs.php
+++ _logs.php
@@ -3,8 +3,10 @@ echo "
 <div class='row'>
   <div class='col-12 py-1 px-4'>
     <div class='card border border-info bg-dark text-white'>
-      <h5 class='card-header bg-info text-white p-1'>Logs</h5>
-      <div class='card-body p-1'>
+      <h5 class='card-header bg-info text-dark p-1'
+      data-bs-toggle='collapse' data-bs-target='#collapselogs'
+      >Logs <small class='text-muted'>Click/Tap to show logs</small></h5>
+      <div class='card-body p-1 collapse' id='collapselogs'>
       ";
 
 foreach ($jsonData['logs'] as $key => $log) echo "<p class='my-1'>{$log[0]}</p>";
