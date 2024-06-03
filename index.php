db816e260d0d5ceba6249610bd2328298a8f2fa3
MrBeardedJoe <goneover@gmail.com>
2024-06-03 09:13:13 -0500

    changes for github language recognition

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once "./template/header.php";
+include_once "./template/header.html";
 $filePath = "./json.json";
 include_once "./_functions.php";
 
@@ -8,4 +8,4 @@ include_once "_stats.php";
 include_once "_list.php";
 include_once "_logs.php";
 
-include_once "./template/footer.php";
+include_once "./template/footer.html";
