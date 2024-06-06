f54ec00fbac39a7bfbc10f2578ab7edc2643d783
MrBeardedJoe <goneover@gmail.com>
2024-06-06 15:42:00 -0500

    line through if invalid and added spacing via css for numbers

--- sections/_list.php
+++ sections/_list.php
@@ -3,7 +3,8 @@
 
     <div class='card border border-success bg-dark text-white'>
       <h6 class='card-header bg-success text-white'>
-        <img src='./images/codes.jpg' alt='Codes: Picture of codes' height='18' class='ms-2' style='margin-top:-0.4rem;border-radius: 3rem;'>
+        <img src='./images/codes.jpg' alt='Codes: Picture of codes' height='18' class='ms-2'
+          style='margin-top:-0.4rem;border-radius: 3rem;'>
         Codes
       </h6>
       <span>
@@ -24,11 +25,12 @@
           <?php
           foreach ($jsonData['codes'] as $code => $data) {
             $ifSuccess = ($data['status'] == "success") ? "data-success-code='$code'" : "";
+            $ifInvalid = ($data['status'] == "invalid") ? "text-decoration-line-through text-muted" : "";
             echo "
             <tr>
-              <td $ifSuccess>$code</td>
-              <td class='no-select'>{$data['status']}</td>
-              <td class='no-select'>{$data['credit']}</td>
+              <td class='font-monospace $ifInvalid' style='letter-spacing: 0.25rem;' $ifSuccess>$code</td>
+              <td class='no-select $ifInvalid'>{$data['status']}</td>
+              <td class='no-select $ifInvalid'>{$data['credit']}</td>
             </tr>";
           }
           ?>
