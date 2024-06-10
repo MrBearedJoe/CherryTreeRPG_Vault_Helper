ae9c697712e0376216bb3f4b0f4060809e6114b6
MrBeardedJoe <goneover@gmail.com>
2024-06-10 12:23:33 -0500

    add to the top button

--- sections/_list.php
+++ sections/_list.php
@@ -1,16 +1,16 @@
   <div class='col-md-6 col-sm-12 py-1 px-4'>
 
     <div class='card border border-success bg-dark text-white'>
-      <h6 class='card-header bg-success text-white'>
-        <img src='./images/codes.jpg' alt='Codes: Picture of codes' height='18' class='ms-2'
-          style='margin-top:-0.4rem;border-radius: 3rem;'>
+      <h6 class='card-header bg-success text-white' data-bs-toggle='collapse' data-bs-target='#collapseCodes'>
+        <img src='./images/codes.jpg' alt='Codes: Picture of codes' height='18' class='ms-2' style='margin-top:-0.4rem;border-radius: 3rem;'>
         Codes
+        <small class='text-muted'>Click/Tap to hide codes</small>
       </h6>
       <!-- <span>
         <input type='checkbox' id='invalidFilter'>
         <label for='invalidFilter'>Hide invalid</label>
       </span> -->
-      <div class='card-body p-0'>
+      <div class='card-body p-0' id='collapseCodes'>
 
         <div id='successCode'></div>
 
